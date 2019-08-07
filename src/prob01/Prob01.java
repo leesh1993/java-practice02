cc3469c2dc2a2adffc80b297a8987ef5712b52a7
BIT <BIT@DESKTOP-08BCIT0>
2019-08-07 19:11:45 +0900

    2019-08-07 practice02

--- src/prob01/Prob01.java
+++ src/prob01/Prob01.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class Prob01 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in  );
+		int save[] = {50000,10000,5000,1000,500,100,50,10,1};
+				
+		Scanner scanner = new Scanner( System.in  );	
+		System.out.print( "±Ý¾× : " );
+		int money = scanner.nextInt(); 
 		
-		System.out.print( "ê¸ˆì•¡: " );
-		int money = scanner.nextInt();
-		
-		scanner.close();
+		for(int i = 0; i < save.length; i++) {
+			int separation = money/save[i];
+			money = money % save[i];
+			System.out.println(save[i] + "¿ø " + separation + "°³");
+		}
  	}
 }
--- src/prob03/Prob03.java
+++ src/prob03/Prob03.java
@@ -3,7 +3,8 @@ package prob03;
 public class Prob03 {
 	public static void main(String args[]) {
 		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
-
+		
+		
 		// 원래 배열 원소 출력
 		printCharArray(c);
 
@@ -15,8 +16,20 @@ public class Prob03 {
 	}
 
 	public static void replaceSpace(char c[]) {
+		String change = new String(c);//change = change + Character.toString(c[i]);
+		for(int i = 0; i < c.length; i++) {
+			if(change.charAt(i) == ' ') {
+				c[i] = ',';
+			}
+			//System.out.print(c[i]);
+		}
+		//System.out.print("\n");
 	}
 
 	public static void printCharArray(char c[]) {
+		for(int i = 0; i < c.length; i++) {
+			System.out.print(c[i]);
+		}
+		System.out.print("\n");
 	}
 }
